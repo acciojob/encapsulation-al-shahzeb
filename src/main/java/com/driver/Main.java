32c965777fc1c850df2e059ceb993549f68c1d6a
al-shahzeb <alishahzeb453@gmail.com>
2023-03-14 14:06:28 +0530

    Encapsulation Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        RWOnly rwOnly= new RWOnly();
+
+        /**
+         * rwOnly.encaps="Encapsulation";
+         * 'encaps' has private access in 'com.driver.RWOnly'
+         **/
+
+
+        rwOnly.setEncaps("This is encapsulation.");
+        System.out.println(rwOnly.getEncaps());
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String encaps;
+
+    public void setEncaps(String encaps) {
+        this.encaps = encaps;
+    }
+
+    public String getEncaps() {
+        return encaps;
+    }
 }
